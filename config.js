9b532fb7c342528072ed59d4b7a0b1ef8fc8ab5e
Ana Pereira <ana.c.pereira@ua.pt>
2026-04-17 01:20:17 +0100

    cleanup: remove debug logging

--- config.js
+++ config.js
@@ -3,7 +3,5 @@
 export const BASE_PATH = window.location.hostname === 'localhost' ? '' : '/RaccoonAdventures_ICG';
 
 export function getAssetPath(relativePath) {
-    const fullPath = `${BASE_PATH}/${relativePath}`;
-    console.log(`getAssetPath("${relativePath}") => "${fullPath}"`);
-    return fullPath;
+    return `${BASE_PATH}/${relativePath}`;
 }
